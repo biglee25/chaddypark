2760bbffc98ba396e85ae690fd3f68c2aad865f1
Lee Attwater <leeattwater@gmail.com>
2020-12-20 14:19:50 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next Starter</title>
+        <title>Next Starters</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
