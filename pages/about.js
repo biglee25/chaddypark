d0d0353e8c811d4724e24a864464ef47d2ca9ce6
Lee Attwater <leeattwater@gmail.com>
2021-06-26 17:37:31 +0100

    fix for cms

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,38 @@
-import Link from 'next/link';
-import { GraphQLClient } from 'graphql-request';
-
-export async function getStaticProps() {
-  const graphcms = new GraphQLClient(
-    'https://api-eu-central-1.graphcms.com/v2/ckpzqrd16959901w561z47ak4/master'
-  );
-
-  const { pages } = await graphcms.request(
-    `
-      { 
-        pages {
-          title
-          subtitle
-        }
-      }
-    `
-  );
-
-  return {
-    props: {
-      pages,
-    },
-  };
+export default function About() {
+  return(
+    <h1>about page</h1>
+  )
 }
+// import Link from 'next/link';
+// import { GraphQLClient } from 'graphql-request';
 
-export default ({ pages }) =>
-  pages.map(({ slug, title, subtitle }) => (
-    <>
-      <p>{title}</p>
-      <p>{subtitle}</p>
-      </>
-  ));
+// export async function getStaticProps() {
+//   const graphcms = new GraphQLClient(
+//     'https://api-eu-central-1.graphcms.com/v2/ckpzqrd16959901w561z47ak4/master'
+//   );
+
+//   const { pages } = await graphcms.request(
+//     `
+//       { 
+//         pages {
+//           title
+//           subtitle
+//         }
+//       }
+//     `
+//   );
+
+//   return {
+//     props: {
+//       pages,
+//     },
+//   };
+// }
+
+// export default ({ pages }) =>
+//   pages.map(({ slug, title, subtitle }) => (
+//     <>
+//       <p>{title}</p>
+//       <p>{subtitle}</p>
+//       </>
+//   ));
