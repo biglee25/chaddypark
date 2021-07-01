3b3873f3db1498391a0d2cf3aae1e9d6bf89703a
Lee Attwater <leeattwater@gmail.com>
2021-07-01 19:00:50 +0100

    added extra profile

--- pages/teams/[slug].js
+++ pages/teams/[slug].js
@@ -17,10 +17,19 @@ export async function getStaticProps({ params }) {
       team(where: { slug: $slug }) {
         slug
         title
+        heading
         content {
           html
         }
         image {
+          height
+          url
+          width
+        }
+        profile {
+            html
+        }
+        profileImage {
           height
           url
           width
@@ -46,6 +55,7 @@ export async function getStaticPaths() {
       teams {
         slug
         title
+        heading
         content {
           html
         }
@@ -54,6 +64,15 @@ export async function getStaticPaths() {
           url
           width
         }
+        profile {
+            html
+        }
+        profileImage {
+          height
+          url
+          width
+        }
+
       }
     }
   `);
@@ -72,18 +91,35 @@ export default ({ team }) => (
   <Layout>
     <Container>
       <div className="text-center">
-        <h1 className="text-6xl font-bold text-center mt-12 pb-5">{team.title}</h1>
+        <h1 className="text-center mt-12">{team.title}</h1>
+        <h2 className="font-bold text-center pb-5">{team.heading}</h2>
         <Image
             src={team.image.url}
             width={team.image.width}
             height={team.image.height}
           />
-        <div dangerouslySetInnerHTML={{ __html: team.content.html }} className="m-auto pt-12" />
+          <div className="bg-yellow-200 my-12 rounded-lg p-5">
+          <div dangerouslySetInnerHTML={{ __html: team.content.html }} className="m-auto py-12" />
+
+          </div>
+
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center bg-gray-100 p-12 rounded-lg">
+        <div className="flex items-center justify-center">
+          <div dangerouslySetInnerHTML={{ __html: team.profile.html }} className="m-auto pt-12 text-black" />
+        </div>
+        <div>
+        <Image
+            src={team.profileImage.url}
+            width={team.profileImage.width}
+            height={team.profileImage.height}
+          />
+        </div>
       </div>
     </Container>
     <div className="w-full m-auto text-center mt-12">
     <Link href="/teams" className="text-black">
-      <button className="bg-primaryyellow hover:bg-yellow-400 px-12 py-5">
+      <button className="bg-primaryyellow hover:bg-yellow-400 w-full md:w-56 px-24 md:px-12 py-5">
         Back to Teams
       </button>
       </Link>
