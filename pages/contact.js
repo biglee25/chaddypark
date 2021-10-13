b1c82ea90ca595d34096ee0c6da623ea3d721e54
Lee Attwater <leeattwater@gmail.com>
2021-10-13 19:05:41 +0100

    card change

--- pages/contact.js
+++ pages/contact.js
@@ -10,8 +10,8 @@ export async function getStaticProps() {
   
   const { contacts } = await graphcms.request(
     `
-    query MyQuery {
-      contacts {
+    query MyQuery  {
+      contacts (orderBy: order_ASC) {
         profileImage {
           url
           width
@@ -42,17 +42,17 @@ export default function Teams({ contacts }) {
       <Container>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 text-center mt-24">
           {contacts.map((contact) => (
-                <div className="container font-bold bg-gray-100 shadow-lg">
+                <div className="container font-bold bg-white shadow-lg h-96">
                   <img
                     src={contact.profileImage.url}
                     width="150px"
                     height="150px"
                     className="rounded-full transform -translate-y-6 mx-auto shadow-lg border-4 border-white"
                   />
-                    <h2 className="uppercase">{contact.name}</h2>
-                    <p className="bg-black text-white mx-12 p-2">{contact.title}</p>
-                    <h2>{contact.telephone}</h2>
-                  <div dangerouslySetInnerHTML={{ __html: contact.email.html }} className="m-auto py-4 bg-gray-900 text-white rounded-b-lg" />
+                    <h2 className="uppercase font-extrabold">{contact.name}</h2>
+                    <p className="font-normal">{contact.title}</p>
+                    <h2 className="text-base font-extrabold">{contact.telephone}</h2>
+                  <div dangerouslySetInnerHTML={{ __html: contact.email.html }} className="m-auto font-normal" />
               </div>
           ))}
         </div>
